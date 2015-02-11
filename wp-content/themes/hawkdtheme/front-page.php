5ca294a9a8f9874b9591a245aa9af62edb1ac259
smurfen <hej@jenserik.se>
2015-02-11 15:06:37 +0100

    ojoj

--- wp-content/themes/hawkdtheme/front-page.php
+++ wp-content/themes/hawkdtheme/front-page.php
@@ -27,7 +27,30 @@ if ( $the_query -> have_posts() ) : while ( $the_query -> have_posts() ) :
 <article id="post-<?php the_ID(); ?>" <?php post_class();?>>
 	<div class="article-body">
 	<h1 class="text-center"><?php print the_title(); ?></h1>
+	<?php
 
+	if (get_the_ID() == 5) :
+		$image = wp_get_attachment_image_src(get_field('image_1'), 'full'); ?>
+	<div class="row">
+			<div class="col-xs-3 col-xs-offset-1 img-box">	
+			<img src="<?php echo $image[0]; ?>" alt="<?php echo get_the_title(get_field('image_1')) ?>" />
+			<p><?php print get_field('text_1'); ?> </p>
+		</div>
+
+		<?php $image = wp_get_attachment_image_src(get_field('image_2'), 'full'); ?>
+		<div class="col-xs-3 col-xs-offset-1 img-box">
+			<img src="<?php echo $image[0]; ?>" alt="<?php echo get_the_title(get_field('image_2')) ?>" />
+			<p><?php print get_field('text_2'); ?> </p>
+		</div>
+		
+		
+		<?php $image = wp_get_attachment_image_src(get_field('image_3'), 'full'); ?>
+		<div class="col-xs-3 col-xs-offset-1 img-box">
+			<img src="<?php echo $image[0]; ?>" alt="<?php echo get_the_title(get_field('image_3')) ?>" />
+			<p><?php print get_field('text_3'); ?> </p>
+		</div>
+	</div>
+	<?php endif; ?>
 	<?php print the_content(); ?>
 	</div>
 </article>
